7998efead6a78a628e78f9f3b7a3cbd289cdbc70
Abe <Abe@Abe-HP>
2016-12-23 02:26:40 +0100

    Constructeur Guides Spirituels

--- Divinae/src/cartes/guide/Anarchiste.java
+++ Divinae/src/cartes/guide/Anarchiste.java
@@ -8,7 +8,8 @@ import cartes.Origine;
 public class Anarchiste extends GuideSpirituel {
   
   public Anarchiste (String nom, Origine origine, String capacite, Dogme[] dogme, int nombreCroyant) {
-	 	super(nom, origine, capacite, dogme, nombreCroyant);
+	 	super("Anarchiste", Origine.Neant,  "Sacrifie un Guide Spirituel, si lui ou sa Divinité ne croit pas au Dogme Chaos."
+	 			+ "Les capacités spéciales sont jouées normalement.", new Dogme [] {Dogme.Humain,Dogme.Chaos}, 3);
 		
 		// TODO Auto-generated constructor stub
 	}
--- Divinae/src/cartes/guide/Devin.java
+++ Divinae/src/cartes/guide/Devin.java
@@ -9,7 +9,8 @@ import cartes.divinite.Divinite;
 public class Devin extends GuideSpirituel {
   
   public Devin (String nom, Origine origine, String capacite, Dogme[] dogme, int nombreCroyant) {
-	 	super(nom, origine, capacite, dogme, nombreCroyant);
+	 	super("Devin", Origine.Neant, "Oblige une Divinité ayant le Dogme Nature ou Mystique à sacrifier"
+	 			+ " l'un de ses Guides Spirituels.", new Dogme [] {Dogme.Nature,Dogme.Mystique}, 1);
 		
 		// TODO Auto-generated constructor stub
 	}
